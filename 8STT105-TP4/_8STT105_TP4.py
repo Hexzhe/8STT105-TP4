0c76e25af29167090b8036958acfbd6af94f1155
Hexzhe <hexzhe@jsngbt.com>
2019-12-14 14:12:03 -0500

    Color tweak. Parameterized pretty much everything.

--- 8STT105-TP4/_8STT105_TP4.py
+++ 8STT105-TP4/_8STT105_TP4.py
@@ -8,16 +8,13 @@ class Modele(object):
     def __init__(self, master = None):
         self.init_modele() #init du modele lui-meme
         #Canvas pour le rendering graphique 
-        self.canvas_size = (600, 600) #Taille du canvas pour le rendering
         if master is not None: #Fenetre de rendering si necessaire
-            self.refreshTk = 1.0
-            self.waitTk = 3
             self.frame = Frame(master)
             self.frame.pack()
             self.bframe = Frame(self.frame)
             self.bframe.pack(side = TOP)
             self.gframe = Frame(self.frame, bd = 2, relief = RAISED)
-            self.g = Canvas(self.gframe, bg = "white", width = self.canvas_size[0], height = self.canvas_size[1]) 
+            self.g = Canvas(self.gframe, bg = self.backgroundColor, width = self.windowSize[0], height = self.windowSize[1]) 
             self.g.pack()
             self.g.bind("<ButtonPress-1>", self.onClick1) #Click 1 (left)
             self.g.bind("<ButtonPress-2>", self.onClick2) #Click 2 (centre)
@@ -29,29 +26,46 @@ class Modele(object):
 
     def onClick2(self, event):
         #On sleep pendant 1 a 60 secondes proportionnel a x, d'un click centre
-        sleep(int(60 * event.x // self.canvas_size[0]) + 1)
+        sleep(int(60 * event.x // self.windowSize[0]) + 1)
 
     def onClick1(self, event):
         #On ralenti l'affichage d'un % proportionnel a x (i.e. 0 a 100%), d'un click left
-        self.refreshTk *= 1.0 + event.x / self.canvas_size[0]
+        self.tick *= 1.0 + event.x / self.windowSize[0]
 
     def onClick3(self, event):
         #On accelere l'affichage d'un % proportionnel a x (i.e. 0 a 100%), d'un click right
-        self.refreshTk *= 0.5 * event.x / self.canvas_size[0]
+        self.tick *= 0.5 * event.x / self.windowSize[0]
 
     def init_modele(self): #Init du modele
-        self.n = 25 #Nombre de pas de simulation
+        self.n = 10 #Nombre de pas de simulation
         self.i = 0 #Variable d'etat
-        self.borderColor = "lawn green"
+        self.borderColor = "medium sea green" #borderColorStart
+        self.borderColorEnd = "indian red"
+        self.borderColorDefault = "gray5"
+        self.borderColorActive = "gold"
+        self.fillColor = "Gray75"
+        self.textColor = "gray5"
+        self.font = "times"
+        self.fontSize = 14
+        self.fontWeight = "bold"
+        self.x = 628 #Start X
+        self.y = 468 #Start Y
+        self.boxSize = 25
+        self.spacing = 2
+        self.windowSize = (1280, 960)
+        self.startWait = 3
+        self.tick = 1.0
+        self.backgroundColor = "white"
+        self.clearAfterEach = False
 
     def update(self): #Update du 
-        pass #TODO: Ajouter la logique pour un step ici
+        self.x += self.boxSize + self.spacing #TODO: Ajouter la logique pour un step ici
 
     def render(self, g): #Rendering du modele dans le canvas Tk g
-        bfont = ("times", 14, "bold")
-        bbox = (100, 100, 125, 125) #TODO: update this in update() to move the block
-        g.create_rectangle(bbox, width = 1, outline = self.borderColor, fill = "gray75")
-        g.create_text((bbox[0] + 12.5, bbox[1] + 12.5), text = str(self.i % 10), font = bfont, fill = "gray5")
+        bfont = (self.font, self.fontSize, self.fontWeight)
+        bbox = (self.x, self.y, self.x + self.boxSize, self.y + self.boxSize)
+        g.create_rectangle(bbox, width = 2, outline = self.borderColor, fill = self.fillColor)
+        g.create_text((bbox[0] + 12.5, bbox[1] + 12.5), text = str(self.i % 10), font = bfont, fill = self.textColor)
 
     def run(self): #Boucle de simulation de la dynamique
         for self.i in range(self.n):
@@ -60,24 +74,26 @@ class Modele(object):
             #Rendering tkinter
             if self.g is not None: 
                 if self.i == self.n - 1:
-                    self.borderColor = "tomato"
+                    self.borderColor = self.borderColorEnd
                 elif self.i > 0:
-                    self.borderColor = "gray5"
+                    self.borderColor = self.borderColorDefault
 
-                #self.g.delete(ALL) #Commenter ceci pour laisser chaque step affiché
+                if self.clearAfterEach:
+                    self.g.delete(ALL)
+
                 self.render(self.g)
                 self.g.update()
-                sleep(self.refreshTk)
+                sleep(self.tick)
 
                 if self.i == 0: #Petite pause au premier pas pour voir l'état initial
-                    sleep(self.waitTk)
+                    sleep(self.startWait)
 
 #A executer seulement si ce n'est pas un import, mais bien un run du code
 if __name__ == '__main__':
     if doRenderTk: #Avec rendering Tk (animation)
         root = Tk()
         root.geometry("+0+0")
-        root.title("simulation")
+        root.title("8STT105-TP4")
     else: 
         root = None
     x = Modele(root) #Creation du modele
