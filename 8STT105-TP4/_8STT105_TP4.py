1cc992b06828f0cf930319a0ecb6f13ad3e7e02c
Hexzhe <hexzhe@jsngbt.com>
2019-12-15 10:32:05 -0500

    Added result output for walk-self-avoiding. Added result folder. Updated gitignore to ignore result files.

--- 8STT105-TP4/_8STT105_TP4.py
+++ 8STT105-TP4/_8STT105_TP4.py
@@ -1,10 +1,12 @@
 import sys
+import os
 from tkinter import *
 from time import sleep
 from secrets import randbelow
 
 doRenderTk = True #Enable graphic rendering
-windowSize = (1280, 960)
+windowSize = "1280x960+0+0"
+canevasSize = (1920, 1080) #The canevas size is larger than the window in case the drawing overflow
 backgroundColor = "white"
 
 class Model(object):
@@ -16,7 +18,7 @@ class Model(object):
             self.bframe = Frame(self.frame)
             self.bframe.pack(side = TOP)
             self.gframe = Frame(self.frame, bd = 2, relief = RAISED)
-            self.g = Canvas(self.gframe, bg = backgroundColor, width = windowSize[0], height = windowSize[1]) 
+            self.g = Canvas(self.gframe, bg = backgroundColor, width = canevasSize[0], height = canevasSize[1]) 
             self.g.pack()
             self.g.bind("<ButtonPress-1>", self.onClick1) #Left click
             self.g.bind("<ButtonPress-2>", self.onClick2) #Middle click
@@ -38,19 +40,18 @@ class Model(object):
         #Speed up from 0 to 100% on right click
         self.tick *= 0.5 * event.x / windowSize[0]
 
-    def initModel(self):
+    def initModel(self): #Change settings here
         #Core
+        self.simulationCount = 5 #Number of valid simulation to run back-to-back
+        self.pauseLength = 3 #The pause between each simulation
         self.tick = 0 #Global speed
-        self.n = 200 #Number of step (while i < n)
+        self.n = 500 #Number of step (while i < n)
+        self.targetN = self.n // 2 #Minimum n to consider the simulation successful
         self.i = 0 #Current step
         self.x = 628 #Start X
         self.y = 468 #Start Y
         self.lineLength = 10 #Determine the x and y move size even in non-graphic mode
-        self.lineSpacing = 2 #Determine the x and y added padding (on top of lineLength) even in non-graphic mode
-        self.points = []
-        self.points.append((self.x, self.y)) #Mark the first point as visited
-        self.isDeadlock = False
-        self.targetLength = self.n / 2
+        self.lineSpacing = 0 #Determine the x and y added padding (on top of lineLength) even in non-graphic mode
 
         #Graphic
         self.orientation = 0 #0=N, 1=W, 2=S, 3=E
@@ -60,6 +61,19 @@ class Model(object):
         self.lineColorEnd = "indian red"
         self.lineColorActive = "gold"
         self.clearAfterEach = False #Disable to see a path forming
+
+        self.points = []
+        self.points.append((self.x, self.y)) #Mark the first point as visited
+        self.isDeadlock = False
+
+    def writeResult(self):
+        f = open("ResourceFiles/Results/result-walk-self-avoiding.csv", "a+")
+
+        if os.stat("ResourceFiles/Results/result-walk-self-avoiding.csv").st_size == 0:
+            f.write("i;targetN;n;startX;startY;endX;endY\n")
+
+        f.write(str(self.i) + ";" + str(self.targetN) + ";" + str(self.n) + ";" + str(self.points[0][0]) + ";" + str(self.points[0][1]) + ";" + str(self.points[len(self.points) - 1][0]) + ";" + str(self.points[len(self.points) - 1][1]) + "\n")
+        f.close()
 
     def checkDeadlock(self):
         if self.orientation == 0: #North
@@ -127,44 +141,48 @@ class Model(object):
         g.create_line(line, width = self.borderWidth, fill = self.lineColor)
 
     def run(self): #Simulation loop
-        finalCount = 0
-        while finalCount < self.targetLength: #Quality loop
-            self.g.delete(ALL)
-            self.initModel()
-            for self.i in range(self.n):
-                if self.g is not None: #Pre-rendering
-                    if self.i - 1 > 0 and not self.clearAfterEach:
-                        self.i -= 1
-                        self.lineColor = self.lineColorDefault
-                        self.render(self.g)
-                        self.i += 1
-                    elif self.clearAfterEach:
-                        self.g.delete(ALL)
-            
-                self.update()
-            
-                if self.g is not None: #Rendering
-                    if self.isDeadlock:
-                        self.lineColor = self.lineColorEnd
+        for j in range(self.simulationCount):
+            finalCount = 0
+            while finalCount < self.targetN: #Quality insurance loop
+                self.g.delete(ALL)
+                self.initModel()
+                for self.i in range(self.n):
+                    if self.g is not None: #Pre-rendering
+                        if self.i - 1 > 0 and not self.clearAfterEach:
+                            self.i -= 1
+                            self.lineColor = self.lineColorDefault
+                            self.render(self.g)
+                            self.i += 1
+                        elif self.clearAfterEach:
+                            self.g.delete(ALL)
+                
+                    self.update()
+                
+                    if self.g is not None: #Rendering
+                        if self.isDeadlock:
+                            self.lineColor = self.lineColorEnd
+                            self.render(self.g)
+                            self.g.update()
+                            break
+
+                        if self.i == self.n - 1:
+                            self.lineColor = self.lineColorEnd
+                        elif self.i > 0:
+                            self.lineColor = self.lineColorActive
+                
                         self.render(self.g)
                         self.g.update()
-                        break
-
-                    if self.i == self.n - 1:
-                        self.lineColor = self.lineColorEnd
-                    elif self.i > 0:
-                        self.lineColor = self.lineColorActive
-            
-                    self.render(self.g)
-                    self.g.update()
-                    sleep(self.tick)
-            finalCount = self.i
+                        sleep(self.tick)
+                finalCount = self.i
+            self.writeResult()
+            sleep(self.pauseLength)
+        quit() #Will throw an exception in debug, but make non-debugging more smooth
 
 #Execute on run, not on import
 if __name__ == '__main__':
     if doRenderTk: #Graphic rendering enabled
         root = Tk()
-        root.geometry("+0+0")
+        root.geometry(windowSize)
         root.title("8STT105-TP4")
     else: 
         root = None
--- 8STT105-TP4/walk-self-avoiding.py
+++ 8STT105-TP4/walk-self-avoiding.py
@@ -1,24 +1,24 @@
 import sys
+import os
 from tkinter import *
 from time import sleep
 from secrets import randbelow
 
 doRenderTk = True #Enable graphic rendering
-windowSize = (1280, 960)
+windowSize = "1280x960+0+0"
+canevasSize = (1920, 1080) #The canevas size is larger than the window in case the drawing overflow
 backgroundColor = "white"
-points = []
 
 class Model(object):
     def __init__(self, master = None):
         self.initModel()
-        points.append((self.x, self.y)) #Mark the first point as visited
         if master is not None: #Graphic rendering
             self.frame = Frame(master)
             self.frame.pack()
             self.bframe = Frame(self.frame)
             self.bframe.pack(side = TOP)
             self.gframe = Frame(self.frame, bd = 2, relief = RAISED)
-            self.g = Canvas(self.gframe, bg = backgroundColor, width = windowSize[0], height = windowSize[1]) 
+            self.g = Canvas(self.gframe, bg = backgroundColor, width = canevasSize[0], height = canevasSize[1]) 
             self.g.pack()
             self.g.bind("<ButtonPress-1>", self.onClick1) #Left click
             self.g.bind("<ButtonPress-2>", self.onClick2) #Middle click
@@ -40,10 +40,13 @@ class Model(object):
         #Speed up from 0 to 100% on right click
         self.tick *= 0.5 * event.x / windowSize[0]
 
-    def initModel(self):
+    def initModel(self): #Change settings here
         #Core
-        self.tick = 0.1 #Global speed
-        self.n = 1000 #Number of step (while i < n)
+        self.simulationCount = 5 #Number of valid simulation to run back-to-back
+        self.pauseLength = 3 #The pause between each simulation
+        self.tick = 0 #Global speed
+        self.n = 500 #Number of step (while i < n)
+        self.targetN = self.n // 2 #Minimum n to consider the simulation successful
         self.i = 0 #Current step
         self.x = 628 #Start X
         self.y = 468 #Start Y
@@ -59,13 +62,54 @@ class Model(object):
         self.lineColorActive = "gold"
         self.clearAfterEach = False #Disable to see a path forming
 
+        self.points = []
+        self.points.append((self.x, self.y)) #Mark the first point as visited
+        self.isDeadlock = False
+
+    def writeResult(self):
+        f = open("ResourceFiles/Results/result-walk-self-avoiding.csv", "a+")
+
+        if os.stat("ResourceFiles/Results/result-walk-self-avoiding.csv").st_size == 0:
+            f.write("i;targetN;n;startX;startY;endX;endY\n")
+
+        f.write(str(self.i) + ";" + str(self.targetN) + ";" + str(self.n) + ";" + str(self.points[0][0]) + ";" + str(self.points[0][1]) + ";" + str(self.points[len(self.points) - 1][0]) + ";" + str(self.points[len(self.points) - 1][1]) + "\n")
+        f.close()
+
+    def checkDeadlock(self):
+        if self.orientation == 0: #North
+            isNorthOk = (self.x, self.y + (self.lineLength + self.lineSpacing)) not in self.points
+            isWestOk = (self.x + (self.lineLength + self.lineSpacing), self.y) not in self.points
+            isSouthOk = False
+            isEastOk = (self.x - (self.lineLength + self.lineSpacing), self.y) not in self.points
+        elif self.orientation == 1: #West
+            isNorthOk = (self.x, self.y + (self.lineLength + self.lineSpacing)) not in self.points
+            isWestOk = (self.x + (self.lineLength + self.lineSpacing), self.y) not in self.points
+            isSouthOk = (self.x, self.y - (self.lineLength + self.lineSpacing)) not in self.points
+            isEastOk = False
+        elif self.orientation == 2: #South
+            isNorthOk = False
+            isWestOk = (self.x + (self.lineLength + self.lineSpacing), self.y) not in self.points
+            isSouthOk = (self.x, self.y - (self.lineLength + self.lineSpacing)) not in self.points
+            isEastOk = (self.x - (self.lineLength + self.lineSpacing), self.y) not in self.points
+        elif self.orientation == 3: #East
+            isNorthOk = (self.x, self.y + (self.lineLength + self.lineSpacing)) not in self.points
+            isWestOk = False
+            isSouthOk = (self.x, self.y - (self.lineLength + self.lineSpacing)) not in self.points
+            isEastOk = (self.x - (self.lineLength + self.lineSpacing), self.y) not in self.points
+
+        return not (isNorthOk or isWestOk or isSouthOk or isEastOk)
+
     def update(self): #Model update after each tick
         previousOrientation = self.orientation
         previousX = self.x
         previousY = self.y
 
+        if self.checkDeadlock():
+            self.isDeadlock = True
+            return
+
         first = True
-        while first or (self.orientation != previousOrientation and (self.orientation % 2 == previousOrientation % 2)) or ((self.x, self.y) in points):
+        while first or ((self.orientation != previousOrientation and (self.orientation % 2 == previousOrientation % 2)) or ((self.x, self.y) in self.points)):
             first = False
             self.orientation = randbelow(4)
 
@@ -82,7 +126,7 @@ class Model(object):
                 self.x = previousX - (self.lineLength + self.lineSpacing)
                 self.y = previousY
 
-        points.append((self.x, self.y))
+        self.points.append((self.x, self.y))
 
     def render(self, g): #Render a box at the current coordinates
         if self.orientation == 0: #North
@@ -96,34 +140,49 @@ class Model(object):
 
         g.create_line(line, width = self.borderWidth, fill = self.lineColor)
 
-    def run(self): #Boucle de simulation de la dynamique
-        for self.i in range(self.n):
-            if self.g is not None: #Pre-rendering
-                if self.i - 1 > 0 and not self.clearAfterEach:
-                    self.i -= 1
-                    self.lineColor = self.lineColorDefault
-                    self.render(self.g)
-                    self.i += 1
-                elif self.clearAfterEach:
-                    self.g.delete(ALL)
+    def run(self): #Simulation loop
+        for j in range(self.simulationCount):
+            finalCount = 0
+            while finalCount < self.targetN: #Quality insurance loop
+                self.g.delete(ALL)
+                self.initModel()
+                for self.i in range(self.n):
+                    if self.g is not None: #Pre-rendering
+                        if self.i - 1 > 0 and not self.clearAfterEach:
+                            self.i -= 1
+                            self.lineColor = self.lineColorDefault
+                            self.render(self.g)
+                            self.i += 1
+                        elif self.clearAfterEach:
+                            self.g.delete(ALL)
+                
+                    self.update()
+                
+                    if self.g is not None: #Rendering
+                        if self.isDeadlock:
+                            self.lineColor = self.lineColorEnd
+                            self.render(self.g)
+                            self.g.update()
+                            break
 
-            self.update() 
-
-            if self.g is not None: #Rendering
-                if self.i == self.n - 1:
-                    self.lineColor = self.lineColorEnd
-                elif self.i > 0:
-                    self.lineColor = self.lineColorActive
-
-                self.render(self.g)
-                self.g.update()
-                sleep(self.tick)
+                        if self.i == self.n - 1:
+                            self.lineColor = self.lineColorEnd
+                        elif self.i > 0:
+                            self.lineColor = self.lineColorActive
+                
+                        self.render(self.g)
+                        self.g.update()
+                        sleep(self.tick)
+                finalCount = self.i
+            self.writeResult()
+            sleep(self.pauseLength)
+        quit() #Will throw an exception in debug, but make non-debugging more smooth
 
 #Execute on run, not on import
 if __name__ == '__main__':
     if doRenderTk: #Graphic rendering enabled
         root = Tk()
-        root.geometry("+0+0")
+        root.geometry(windowSize)
         root.title("8STT105-TP4")
     else: 
         root = None
